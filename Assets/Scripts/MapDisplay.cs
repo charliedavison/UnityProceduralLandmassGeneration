39daaef71cc773881e3355bd784d51a91c87d5bf
Charles Davison <charlesdavison@gmail.com>
2018-03-23 20:12:26 +0000

    Noise generation

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapDisplay : MonoBehaviour {
+public class MapDisplay : MonoBehaviour
+{
 
     public Renderer textureRenderer;
 
@@ -15,10 +16,19 @@ public class MapDisplay : MonoBehaviour {
 
         Color[] colourMap = new Color[width * height];
 
-        for(int y = 0; y< height; y++)
+        for (int y = 0; y < height; y++)
         {
-
+            for (int x = 0; x < width; x++)
+            {
+                colourMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+            }
         }
+
+        texture.SetPixels(colourMap);
+        texture.Apply();
+
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(width, 1, height);
     }
-	
+
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,10 +7,14 @@ public class MapGenerator : MonoBehaviour {
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
+    public bool autoUpdate;
 
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
     }
 
 }
