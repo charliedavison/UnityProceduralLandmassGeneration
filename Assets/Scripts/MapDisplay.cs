fe1134fadff71eccace126f8f9f40c8786b49121
Charles Davison <charlesdavison@gmail.com>
2018-03-23 19:58:18 +0000

    Initial Commit

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour {
+
+    public Renderer textureRenderer;
+
+    public void DrawNoiseMap(float[,] noiseMap)
+    {
+        int width = noiseMap.GetLength(0);
+        int height = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(width, height);
+
+        Color[] colourMap = new Color[width * height];
+
+        for(int y = 0; y< height; y++)
+        {
+
+        }
+    }
+	
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+    }
+
+}
