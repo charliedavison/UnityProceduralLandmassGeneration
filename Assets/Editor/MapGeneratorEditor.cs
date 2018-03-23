a97b710d6c60d6baa38012732b0a62c486063b9d
Charles Davison <charlesdavison@gmail.com>
2018-03-23 21:06:24 +0000

    Colour maps

--- Assets/Editor/MapGeneratorEditor.cs
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -6,22 +6,18 @@ using UnityEditor;
 [CustomEditor(typeof(MapGenerator))]
 public class MapGeneratorEditor : Editor {
 
-    public override void OnInspectorGUI()
-    {
+    public override void OnInspectorGUI() {
         MapGenerator mapGen = (MapGenerator)target;
 
-        if(DrawDefaultInspector())
-        {
-            if (mapGen.autoUpdate)
-            {
+        if (DrawDefaultInspector()) {
+            if (mapGen.autoUpdate) {
                 mapGen.GenerateMap();
             }
         }
 
-        if (GUILayout.Button("Generate"))
-        {
+        if (GUILayout.Button("Generate")) {
             mapGen.GenerateMap();
-        } 
+        }
     }
 
 }
